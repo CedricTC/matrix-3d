a43ad8ca6d147cc193aea69ca8eb4074f168bc5d
CedricTC <furkan.akbas@ogr.altinbas.edu.tr>
2025-07-19 17:08:11 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// Ortam değişkenini kontrol et
-const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  // Sadece üretim ortamında 'base' ayarını kullan
-  base: isProduction ? "/matrix-3d-new/" : '/', // Yerelde kök dizini kullan
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+  plugins: [react(),tailwindcss()], 
+  
+      
   }
-})
+);
